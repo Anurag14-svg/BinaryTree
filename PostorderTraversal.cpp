326289ff0b1f849cc9888561698eadfd164cb54c
Anurag14-svg <84181319+Anurag14-svg@users.noreply.github.com>
2022-01-28 05:32:58 -0800

    Update PostorderTraversal.cpp

--- PostorderTraversal.cpp
+++ PostorderTraversal.cpp
@@ -16,3 +16,69 @@ private:
         output.push_back(root->val);
     }
 };
+
+
+// Iterative way with two stack 
+
+class Solution {
+
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> postorder;
+        if(root == NULL) return postorder;
+        stack<TreeNode*> st1, st2; 
+        st1.push(root); 
+        while(!st1.empty()) {
+            root = st1.top(); 
+            st1.pop();
+            st2.push(root); 
+            if(root->left != NULL) {
+                st1.push(root->left); 
+            }
+            if(root->right != NULL) {
+                st1.push(root->right); 
+            }
+        }
+        while(!st2.empty()) {
+            postorder.push_back(st2.top()->val); 
+            st2.pop(); 
+        }
+        return postorder;
+    }
+};
+
+
+// Iterative way using single stack 
+
+class Solution {
+
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> postorder;
+        if(root == NULL) return postorder;
+        stack<TreeNode*> st1;
+        TreeNode* current = root;  
+        while(current != NULL || !st1.empty()) {
+            if(current != NULL){
+                st1.push(current);
+                current = current->left;
+            }else{
+                TreeNode* temp = st1.top()->right;
+                if (temp == NULL) {
+                    temp = st1.top();
+                    st1.pop(); 
+                    postorder.push_back(temp->val);
+                    while (!st1.empty() && temp == st1.top()->right) {
+                        temp = st1.top();
+                        st1.pop(); 
+                        postorder.push_back(temp->val);
+                    }
+                } else {
+                    current = temp;
+                }
+            }
+        }
+        return postorder;
+    }
+};
+
