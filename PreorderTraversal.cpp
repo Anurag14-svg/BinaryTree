79af17b9ad3818f439ddcd5b0da0d33ff8479b0f
Anurag14-svg <84181319+Anurag14-svg@users.noreply.github.com>
2022-01-28 04:24:37 -0800

    Update PreorderTraversal.cpp

--- PreorderTraversal.cpp
+++ PreorderTraversal.cpp
@@ -7,3 +7,33 @@ void preorder(TreeNode *root ){
         preorder(root->left);
         preorder(root->right);
     }
+
+
+// Iterative approach 
+
+vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root==NULL){
+            return ans;
+        }
+        stack<TreeNode* > st;
+        
+        st.push(root);
+        
+        while(!st.empty()){
+            TreeNode* top = st.top();
+            st.pop();
+            ans.push_back(top->val);
+            
+            if(top->right != NULL){
+                st.push(top->right);
+            }
+            if(top->left != NULL){
+                st.push(top->left);
+            }
+            
+            
+        }
+        
+        return ans;
+    }
